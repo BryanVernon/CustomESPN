367354a7043fdd3149bc6c159b2dd5d464115cbe
BryanVernon <bryan.vernon@hotmail.com>
2024-09-20 20:14:13 -0500

    added dependency

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,14 @@ import rankingsRouter from './routes/rankings.js'; // Adjust the path based on y
 import bettingRouter from './routes/betting.js'; // Adjust the path based on your folder structure
 import mediaRouter from './routes/media.js'; // Adjust the path based on your folder structure
 import teamsRouter from './routes/teams.js'; // Adjust the path based on your folder structure
+
 const app = express();
 const port = process.env.PORT || 3101;
+
+// Middleware
+app.use(cors());
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 // Connect to MongoDB
 const mongoUri = process.env.MONGODB_URI;
@@ -20,13 +26,16 @@ MongoClient.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true 
     dbClient = client;
     db = client.db('ncaa');
     console.log('Connected to MongoDB');
-  })
-  .catch(error => console.error('Error connecting to MongoDB:', error));
 
-// Middleware
-app.use(cors());
-app.use(express.json()); // for parsing application/json
-app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+    // Start the server after successful connection
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+  })
+  .catch(error => {
+    console.error('Error connecting to MongoDB:', error);
+    process.exit(1); // Exit the process if MongoDB connection fails
+  });
 
 // Set up the MongoDB client for routes
 app.use((req, res, next) => {
@@ -35,7 +44,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// Use the games router
+// Use the routers
 app.use('/api/games', gamesRouter);
 app.use('/api/records', recordsRouter);
 app.use('/api/rankings', rankingsRouter);
@@ -43,10 +52,7 @@ app.use('/api/betting', bettingRouter);
 app.use('/api/media', mediaRouter);
 app.use('/api/teams', teamsRouter);
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+// Welcome route
 app.get('/', (req, res) => {
   res.send('Welcome to the Custom ESPN API!');
 });
